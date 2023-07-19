0afc98bd3393ac9035b9cf0c1f617af149546051
ddd-dong <dong1214.mailbox@gmail.com>
2023-07-19 10:54:23 +0800

    add readME and update text_filter

--- Ngram_news.py
+++ Ngram_news.py
@@ -8,7 +8,7 @@ DATA_sorce = path+'/WebNews.json'
 update_data='''
 先斷詞，對於中文語意理解
 '''
-text_filter = re.compile(r"[^\u4e00-\u9fa5]")
+text_filter = re.compile(r"[^\u4e00-\u9fa5,。，！？；：「」『』《》【】（）〔〕〈〉“”‘’‛‟…—–\-—]")
 with open(DATA_sorce,encoding='utf8') as file:
     text_data_unprocess=json.load(file) 
     text_data=[]
@@ -16,20 +16,20 @@ with open(DATA_sorce,encoding='utf8') as file:
         text_data.append(re.sub(text_filter,"",i['detailcontent'])) 
 update_data = [re.sub(text_filter,"",update_data)]
 
-# trigram_model=Ngram(text_data,3)
-# trigram_model.generate()
-# trigram_model.save("news",path)
+trigram_model=Ngram(text_data,3)
+trigram_model.generate()
+trigram_model.save("news",path)
 
-trigram_model = Ngram()
-trigram_model.load("news",path)
-trigram_model.update(update_data)
-print(trigram_model.predict_detail("中文"))
+# trigram_model = Ngram()
+# trigram_model.load("news",path)
+# trigram_model.update(update_data)
+# print(trigram_model.predict_detail("中文"))
 
-# word = "早安"
-# print(word,end='')
-# for i in range(1000):
-#     _w = trigram_model.predict(word)
-#     word = word[:-1]+ _w
-#     print(_w,end="")
+word = "早安"
+print(word,end='')
+for i in range(1000):
+    _w = trigram_model.predict(word)
+    word = word[:-1]+ _w
+    print(_w,end="")
 
 
---
+++ README.md
@@ -0,0 +1,61 @@
+# N_Gram
+
+## index
+
+
+````
+│   README.md               說明書
+│   Ngram.py                Ngram object    
+│   Ngram_news.py           demo python script
+|   WebNews.json            demo data(from:https://data.gov.tw/dataset/25891) 
+````
+
+## usage
+creat a N_Gram object
+```
+testmodel = Ngram(train_data,N)
+```
+
+load model
+```
+testmodel = Ngram()
+testmodel.load(file_name,path)
+```
+
+## method
+```
+Name: generate
+function: generate n_gram table
+input: -
+output: -
+```
+```
+Name: update
+function: update n_gram table
+input: update data
+output: -
+```
+```
+Name: save
+function: save n_gram table
+input: file name, save path
+output: -
+```
+```
+Name: load
+function: load n_gram table
+input: file name, save path
+output: -
+```
+```
+Name: predict_detail
+function: use n_gram table to predict next word, return posible words and their posibility
+input: word,n(how many posible words return)
+output: a list of posible words and their posibility, if word not in n_gram table will return None
+```
+```
+Name: predict
+function: use n_gram table to predict next word
+input: word
+output: posible next word, if word not in n_gram table will return a random word
+```
